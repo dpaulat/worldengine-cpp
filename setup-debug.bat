f2513c67b65b38c41ff4c9357db0de77352843cb
Dan Paulat <dpaulat@gmail.com>
2021-03-05 07:39:05 -0600

    Using cmake-conan

--- setup-debug.bat
+++ setup-debug.bat
@@ -3,6 +3,5 @@ pip install numpy
 conan profile new default --detect
 mkdir build-debug
 cd build-debug
-conan install .. -g cmake_find_package -g cmake_paths -s build_type=Debug --build=missing
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CONFIGURATION_TYPES=Debug
 pause
--- setup-release.bat
+++ setup-release.bat
@@ -3,6 +3,5 @@ pip install numpy
 conan profile new default --detect
 mkdir build-release
 cd build-release
-conan install .. -g cmake_find_package -g cmake_paths -s build_type=Release --build=missing
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CONFIGURATION_TYPES=Release
 pause
