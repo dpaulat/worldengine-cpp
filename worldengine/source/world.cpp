c4b14e713f5b72334fefb8c28522c9a954edd9d1
Dan Paulat <dpaulat@gmail.com>
2021-05-07 23:58:56 -0500

    Ignorning protobuf warnings

--- worldengine/source/world.cpp
+++ worldengine/source/world.cpp
@@ -10,7 +10,14 @@
 
 #include <hdf5/H5Cpp.h>
 
+#pragma warning(push)
+#pragma warning(disable : 6385)
+#pragma warning(disable : 6387)
+#pragma warning(disable : 26451)
+#pragma warning(disable : 26495)
+#pragma warning(disable : 26812)
 #include <World.pb.h>
+#pragma warning(pop)
 
 namespace std
 {
